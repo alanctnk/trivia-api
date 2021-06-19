1779127671ba203ee51c9fd579b74b7381641d13
Alan Tanaka <alanctnk@gmail.com>
2021-06-19 17:16:43 -0300

    build commit

---
+++ build/precache-manifest.9187016465d76ae193b7eff505ee1750.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90bbb0cb8174b417c4a9b9413c2b02d",
+    "url": "/trivia-api/index.html"
+  },
+  {
+    "revision": "776bd4be636c98971c31",
+    "url": "/trivia-api/static/css/2.84b3ebfc.chunk.css"
+  },
+  {
+    "revision": "8cb007b3bca9fe25a5e3",
+    "url": "/trivia-api/static/css/main.8513f882.chunk.css"
+  },
+  {
+    "revision": "776bd4be636c98971c31",
+    "url": "/trivia-api/static/js/2.d8f0480a.chunk.js"
+  },
+  {
+    "revision": "67f3a6621581faa63f1113ad3e4e3559",
+    "url": "/trivia-api/static/js/2.d8f0480a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8cb007b3bca9fe25a5e3",
+    "url": "/trivia-api/static/js/main.5d3f1baf.chunk.js"
+  },
+  {
+    "revision": "6e1cbed45b223c0862c9",
+    "url": "/trivia-api/static/js/runtime-main.0fbe0f3c.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-api/static/media/trivia.466d153e.png"
+  }
+]);
