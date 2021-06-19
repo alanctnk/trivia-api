15d866dceab49c6256883c11ce278cbebc495e06
Alan Tanaka <alanctnk@gmail.com>
2021-06-19 19:01:14 -0300

    keep try 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import quizLogo from '../trivia.png';
 import Login from '../components/Login';
 
 class Home extends Component {
   render() {
     return (
       <div className="homeLogin">
-        <img src={ quizLogo } width="400" alt="trivia logo" />
+        <img src="https://upload.wikimedia.org/wikipedia/en/2/27/Trivia.png" width="400" alt="trivia logo" />
         <Login />
       </div>
     );
